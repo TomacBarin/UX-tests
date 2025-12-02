2df317d216e9a61699c9dbfe22f6ba2f240c6678
Tomac Barin Jansson <tomacbarin@me.com>
2025-12-02 07:44:06 +0100

    Mer träning.

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
-async function fetchTemp() {
+async function fetchWeather() {
     try {
-        const response = await fetch("https://api.open-meteo.com/v1/forecast?latitude=56.05&longitude=12.70&current_weather=true");
+    const response = await fetch("https://api.open-meteo.com/v1/forecast?latitude=56.05&longitude=12.70&current_weather=true");
 
-        const data = await response.json();
+    const data = await response.json();
 
-        const temp = data.current_weather.temperature;
-        document.body.innerHTML += `Detta är tempen: ${temp}, så att det, haom!`;
-
+    const temp = data.current_weather.temperature;
+    document.body.innerHTML += `Temperaturen är: ${temp}`;
     } catch(error) {
-        console.log(error)
-        document.body.innerHTML = `Något gick knas: ${error}`;
+        console.error(`Något gick knas: ${error}`);
+        document.body.innerHTML += `Något är fel. ${error}`;
     }
 }
 
-fetchTemp()
+fetchWeather()
