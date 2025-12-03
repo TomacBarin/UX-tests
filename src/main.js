73621826b13918237e374f55ae6d007560e3719a
Tomac Barin Jansson <tomacbarin@me.com>
2025-12-03 07:23:22 +0100

    More practice.

--- src/main.js
+++ src/main.js
@@ -1,2 +1,11 @@
 // main.js
 
+import { Car } from "./modules/Car.js";
+
+const garageHtml = document.querySelector("#garage");
+
+// Tre bilar:
+const car1 = new Car("Ford", "vit", "automat");
+const car2 = new Car("Toyota", "blå", "växellåda");
+const car3 = new Car("Mazda", "beige", "automat");
+
--- src/modules/Car.js
+++ src/modules/Car.js
@@ -1,2 +1,24 @@
 // Car.js
 
+export class Car {
+    constructor(brand, color, transmission) {
+        this.brand = brand;
+        this.color = color;
+        this.transmission = transmission;
+    }
+
+    honk() {
+        console.log(`Min ${this.brand} är ${this.color} och ${this.transmission}.`)
+    }
+
+    carInfo() {
+        return `
+        <div>
+        <h2>${this.brand}</h2>
+        <p>Min ${this.brand} är ${this.color} och ${this.transmission}.</p>
+        </div>
+        `
+    }
+
+
+}
