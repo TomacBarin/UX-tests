18d08313bfd92c93508d8d3e24f3b4ec3f002e23
Tomac Barin Jansson <tomacbarin@me.com>
2025-12-03 06:34:35 +0100

    DOM practice

--- src/main.js
+++ src/main.js
@@ -1,7 +1,2 @@
-import { Car } from "./modules/Car.js";
+// main.js
 
-const car1 = new Car("Yugo", "rostiga")
-console.log(`Min ${car1.color} ${car1.brand} är familjens stolthet.`)
-const car2 = new Car("Lada Riva", "röda")
-console.log(`Min ${car2.color} ${car2.brand} gör alla Tesla-ägare gröna av avund.`)
-car1.honk()
--- src/modules/Car.js
+++ src/modules/Car.js
@@ -1,9 +1,2 @@
-export class Car {
-    constructor(brand, color) {
-        this.brand = brand
-        this.color = color
-    }
-    honk() {
-        console.log(`Den ${this.color} ${this.brand} säger: TUT TUT, WHEEE!`)
-    }
-}
+// Car.js
+
